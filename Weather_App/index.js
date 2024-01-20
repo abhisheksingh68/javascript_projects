798297e761743846a7e3caa1c1d39b52d5fc6c8f
abhishek singh <abhi212068@gmail.com>
2024-01-20 23:58:01 +0530

    create a weather App

---
+++ Weather_App/index.js
@@ -0,0 +1,40 @@
+const apikey = "e3d43a49c9834cf7d9cbf20b55a3b19a";
+
+const  weatherDataEl = document.getElementById("weather-data");
+
+const  cityInputEl = document.getElementById("city-input");
+
+const formEl = document.querySelector("form");
+
+formEl.addEventListener("submit",(event) => {
+	event.preventDefault();
+	const cityValue = cityInputEl.value;
+	console.log(cityValue);
+	getWeatherData(cityValue)
+});
+
+async  function getWeatherData(cityValue){
+	try{
+ const response =await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`)
+
+ if(!response.ok){
+	throw new Error(" Network response was not ok ")
+ }
+
+ const data  = await response.json();
+const temperature = Math.round(data.main.temp)
+
+const description =data.weather[0].description
+const icon = data.weather[0].icon
+
+const details = [
+	`Feels like : ${Math.round(data.main.feels_like)}`,
+	`Humidity : ${data.main.humidity}`,
+	`Wind speed : ${data.wind.speed}`,
+]
+
+weatherDataEl.querySelector(".icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">`
+	}catch (error) {
+
+	}
+}
